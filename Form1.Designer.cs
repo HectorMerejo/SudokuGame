71f51479a10eb7acb097b3b0a2ec164dc03cb582
Hector Merejo <Hector.Merejo@mail.citytech.cuny.edu>
2023-02-09 14:13:36 -0500

    Sudoku
    A program that replicates a simple version of the Sudoku game

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace SudokuGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label1.Location = new System.Drawing.Point(351, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(764, 108);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SUDOKU GAME";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(1632, 1423);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics mygraphics = e.Graphics;
+            SolidBrush mybrush = new SolidBrush(Color.Black);
+            mygraphics.FillRectangle(mybrush, 140, 75, 470, 470);
+            Boolean blue = true;
+            int x, y;
+            x = 153;
+            y = 85;
+            for (int j = 1; j <= 9; j++)
+            {
+                for (int i = 1; i <= 9; i++)
+                {
+
+                    if (blue)
+                    {
+                        mybrush = new SolidBrush(Color.Blue);
+                    }
+
+                    mygraphics.FillRectangle(mybrush, x, y, 45, 45);
+                    x = x + 50;
+                    blue = !blue;
+                }
+                y = y + 50;
+                x = 175;
+                blue = !blue;
+            }
+
+        }
+    }
+}
+
